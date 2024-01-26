46f69055566980ec099818047491fb471998630f
Ketamic <86025681+Ketamic@users.noreply.github.com>
2024-01-25 23:20:47 -0800

    setup OutputTiles() to actually output all tiles

--- GameBoy Emulator/PPU.cpp
+++ GameBoy Emulator/PPU.cpp
@@ -12,16 +12,31 @@ void PPU::init(Memory* nmemory, platform* nplat) {
 // Just spits out all the tiles in VRAM to the screen
 // More of a testing feature than a useful one
 void PPU::OutputTiles() {
-	printf("outputting tiles...");
-	for(int k = 0; )
-	for (int i = 0; i < 0x10; i += 2) {
-		std::uint8_t mem = memory->read(0x8010 + i);
-		for (int j = 0; j < 8; ++j) {
-			if (((mem >> j) & 1U) == 1) {
-				plat->SetScreenArray(8 - j, i / 2, 0xFF000000);
+	int x_index = 0;
+	int y_index = 0;
+	int k_index = 1;
+	if (memory->read(0x8010) != 0) {
+		printf("outputting tiles...\n");
+		for (int k = 0x8000; k < 0x9FFF; k += 0x10) {
+			printf("K: 0x%X", k);
+			for (int i = 0; i < 0x10; i += 2) {
+				std::uint8_t mem = memory->read(k + i);
+				for (int j = 0; j < 8; ++j) {
+					if (((mem >> j) & 1U) == 1) {
+						plat->SetScreenArray((8 - j) + x_index, (i / 2) + y_index, 0xFF000000);
+					}
+					else {
+						plat->SetScreenArray((8 - j) + x_index, (i / 2) + y_index, 0xFFFFFFFF);
+					}
+				}
+			}
+			if (k_index == (k % 0x8000) / 100) {
+				x_index = 0;
+				y_index += 9;
+				++k_index;
 			}
 			else {
-				plat->SetScreenArray(8 - j, i / 2, 0xFFFFFFFF);
+				x_index += 9;
 			}
 		}
 	}
@@ -34,5 +49,4 @@ void PPU::StepPPU(int cycles) {
 		// After a certain amount of cycles
 		memory->write(0xFF44, (memory->read(0xFF44) + 1) % 153);
 	}
-	OutputTiles();
 }
